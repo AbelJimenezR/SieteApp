3ac0973a3bba419a50204839881fb88cf3414386
Abel <abeljimenez@outlook.com>
2026-05-07 18:45:38 +0200

    refactor

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,10 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-importScripts("./service-worker.js");
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
+});
+
